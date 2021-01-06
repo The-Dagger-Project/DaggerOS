8370088d01caaee48859d07336c4e32b42582628
Ayman Hazzam <ayman@hazzam.ga>
2021-01-06 17:53:35 +0100

    Pull request #1 improving

--- kernel.c
+++ kernel.c
@@ -88,9 +88,10 @@ void terminal_putchar(char c)
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+		if (c == "\n")
+			terminal_row ++;
+		terminal_column = 0 ;
+	} 
 }
  
 void terminal_write(const char* data, size_t size) 
