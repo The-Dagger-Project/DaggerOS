8bb609485c54c2870efca74d849810284693db96
Adam Salhi <adam06genius@gmail.com>
2021-01-14 12:40:23 +0000

    Centered everything

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include "../arch/i386/vga.h"
+
+
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("    ___                                ___  __ \n");
-	printf("   /   \\__ _  __ _  __ _  ___ _ __    /___\\/ _\\ \n");
-	printf("  / /\\ / _` |/ _` |/ _` |/ _ \\ '__|  //  //\\ \\ \n");
-	printf(" / /_// (_| | (_| | (_| |  __/ |    / \\_// _\\ \\ \n");
-	printf("/___,' \\__,_|\\__, |\\__, |\\___|_|    \\___/  \\__/\n");
-	printf("             |___/ |___/            \n");
-	printf("\n\n\nMade by  Ayman0x03 & aa2006\n");
+	terminal_setcolor(VGA_COLOR_LIGHT_BLUE);
+	printf("                   ___                                ___  __ \n");
+	printf("                  /   \\__ _  __ _  __ _  ___ _ __    /___\\/ _\\ \n");
+	printf("                 / /\\ / _` |/ _` |/ _` |/ _ \\ '__|  //  //\\ \\ \n");
+	printf("                / /_// (_| | (_| | (_| |  __/ |    / \\_// _\\ \\ \n");
+	printf("               /___,' \\__,_|\\__, |\\__, |\\___|_|    \\___/  \\__/\n");
+	printf("                            |___/ |___/            \n");
+	terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+	printf("\n\n\n                           Made by aa2006 & Ayman0x03\n");
 }
